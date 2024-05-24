998688a1b46459ae84fd1e7cbac4a1cfcabb2396
eddieoz <osoriojr@gmail.com>
2024-05-24 19:51:40 +0300

    Update repeat_bot

--- utils/repeat_bot.py
+++ utils/repeat_bot.py
@@ -16,29 +16,34 @@ def read_last_n_lines(file_path, n):
         return deque(file, n)
 
 def repeat(directory, n_lines, language):
-    latest_file = find_latest_file(directory)
-    if latest_file:
-        last_lines = read_last_n_lines(latest_file, n_lines)
-        message = ''
-        for line in last_lines:
-            message += line.rstrip()     
-        # Point to the local server
-        client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+    try:
+        latest_file = find_latest_file(directory)
+        if latest_file:
+            last_lines = read_last_n_lines(latest_file, n_lines)
+            message = ''
+            for line in last_lines:
+                message += line.rstrip()     
+            # Point to the local server
+            client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
 
-        completion = client.chat.completions.create(
-            model="lmstudio-ai/gemma-2b-it-GGUF",
-            messages=[
-                {"role": "system", "content": "Summarize the content into 3 sentences that capture the main ideas and key points. Translate to " + language + ": "},
-                {"role": "user", "content": message}
-            ],
-            temperature=0.9,
-            max_tokens=200,
-            stream=False
-        )
+            completion = client.chat.completions.create(
+                model="lmstudio-ai/gemma-2b-it-GGUF",
+                messages=[
+                    {"role": "system", "content": "Task: Summarize the following content. Lenght: 3-5 sentences. Requirements: capture the main ideas and key points; easy to understand; summary should be free from ambiguity. Summarize in  " + language + " language. Content: "},
+                    {"role": "user", "content": message}
+                ],
+                temperature=0.6,
+                max_tokens=100,
+                stream=False
+            )
 
-        return completion.choices[0].message.content.replace('\n',' ')
-    else:
-        print("No files found in the directory.")
+            return completion.choices[0].message.content.replace('\n',' ')
+        else:
+            print("No files found in the directory.")
+            return "Estou confuso..."
+    except Exception as e:
+            print(f'Error sending alert: {e}')
+            return "Estou confuso..."
 
 
 
