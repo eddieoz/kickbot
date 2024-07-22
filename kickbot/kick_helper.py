15b1c060b2829b8bb098a0b6fea02b9491f6958e
eddieoz <eddieoz@pm.me>
2024-07-22 19:32:12 +0300

    Update websocket address

--- kickbot/kick_helper.py
+++ kickbot/kick_helper.py
@@ -147,7 +147,8 @@ def get_ws_uri() -> str:
 
     :return: kicks websocket url
     """
-    return 'wss://ws-us2.pusher.com/app/eb1d5f283081a78b932c?protocol=7&client=js&version=7.6.0&flash=false'
+    # return 'wss://ws-us2.pusher.com/app/eb1d5f283081a78b932c?protocol=7&client=js&version=7.6.0&flash=false'
+    return 'wss://ws-us2.pusher.com/app/32cbd69e4b950bf97679?protocol=7&client=js&version=7.6.0&flash=false'
 
 
 #################################################################################################
